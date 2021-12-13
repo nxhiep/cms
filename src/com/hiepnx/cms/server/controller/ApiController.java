7461538accf5ef4115307610e39ec36cc79ba03f
Hiep Nguyen <nxhiep.1996@gmail.com>
2021-12-13 20:03:09 +0700

    x

--- src/com/hiepnx/cms/server/controller/ApiController.java
+++ src/com/hiepnx/cms/server/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.hiepnx.cms.server.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,17 +26,17 @@ import com.hiepnx.cms.shared.model.UserInfo;
 @Controller
 @RequestMapping(value = "/api")
 public class ApiController extends BasicController {
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public @ResponseBody UserInfo login(@RequestParam("account") String account, @RequestParam("password") String password,
-			HttpServletRequest request, HttpServletResponse response) {
+	public @ResponseBody UserInfo login(@RequestParam("account") String account,
+			@RequestParam("password") String password, HttpServletRequest request, HttpServletResponse response) {
 		return USER_DAO.login(account, password, request, response);
 	}
-	
+
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public @ResponseBody UserInfo register(@RequestParam("account") String account, @RequestParam("password") String password,
-			@RequestParam("email") String email, @RequestParam("phoneNumber") String phoneNumber,
-			HttpServletRequest request, HttpServletResponse response) {
+	public @ResponseBody UserInfo register(@RequestParam("account") String account,
+			@RequestParam("password") String password, @RequestParam("email") String email,
+			@RequestParam("phoneNumber") String phoneNumber, HttpServletRequest request, HttpServletResponse response) {
 		final UserInfo newUserInfo = new UserInfo();
 		newUserInfo.setAccount(account);
 		newUserInfo.setPassword(password);
@@ -43,23 +44,23 @@ public class ApiController extends BasicController {
 		newUserInfo.setPhoneNumber(phoneNumber);
 		return USER_DAO.register(newUserInfo, request, response);
 	}
-	
-	@RequestMapping(value="/logout", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/logout", method = RequestMethod.POST)
 	public @ResponseBody void logout(HttpServletRequest request, HttpServletResponse response) {
 		USER_DAO.logout(request, response);
 	}
-	
-	@RequestMapping(value="/get-all-categories", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/get-all-categories", method = RequestMethod.POST)
 	public @ResponseBody List<Category> getAllCategories() {
 		return new CategoryDAO().getAllCategories();
 	}
-	
-	@RequestMapping(value="/get-all-cards", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/get-all-cards", method = RequestMethod.POST)
 	public @ResponseBody List<Card> getAllCards(HttpServletRequest request, HttpServletResponse response) {
 		return new CardDAO().getAllCards();
 	}
-	
-	@RequestMapping(value="/get-all-choices", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/get-all-choices", method = RequestMethod.POST)
 	public @ResponseBody List<Choice> getAllChoices(HttpServletRequest request, HttpServletResponse response) {
 		return new CardDAO().getAllChoices();
 	}
@@ -97,5 +98,12 @@ public class ApiController extends BasicController {
 			return "OK";
 		}
 		return "Not OK";
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public @ResponseBody String test(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String json = "{\"type\":\"pageHistory\",\"data\":{\"url\":\"https://bc-dev-v2-wendy08.myshopify.com/vi/collections/jackets\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36\",\"title\":\"\\n    Jackets – bc-dev-v2-wendy08\\n  \"}}";
+		String url = "https://zozoserver.vercel.app/api/data/update-data";
+		return UrlFetcher.post(url, json);
 	}
 }
